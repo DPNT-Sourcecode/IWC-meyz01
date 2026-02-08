92586441f672724a9aa2d06a71e618f19f46d148
sbx_user1051 <sbx_user1051@169.254.3.27>
2026-02-08 20:59:07 +0000

    Sun Feb 08 20:59:07 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -10,15 +10,16 @@ def test_enqueue_size_dequeue_flow() -> None:
         call_dequeue().expect("companies_house", 1),
     ])
     
-    # Multiple user tasks
+    # Rule of 3 ordering
     run_queue([
-        call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
-        call_enqueue("id_verification", 1, iso_ts(delta_minutes=10)).expect(2),
+        call_enqueue("companies_house", 1).expect(1),
+        call_enqueue("id_verification", 1).expect(2),
 
-        call_enqueue("companies_house", 1, iso_ts(delta_minutes=20)).expect(2),
-        call_enqueue("companies_house", 1, iso_ts(delta_minutes=20)).expect(2),
-        call_enqueue("companies_house", 1, iso_ts(delta_minutes=10)).expect(3),
-        call_size().expect(3),
+        call_enqueue("id_verification", 2).expect(2),
+        call_enqueue("id_verification", 3).expect(2),
+
+        call_enqueue("bank_statements", 1).expect(2),
+        
         call_dequeue().expect("companies_house", 1),
         call_dequeue().expect("companies_house", 1),
         call_dequeue().expect("companies_house", 1),
@@ -27,13 +28,14 @@ def test_enqueue_size_dequeue_flow() -> None:
     # Temporal ordering for 1 user
     run_queue([
         call_enqueue("companies_house", 1, iso_ts(delta_minutes=30)).expect(1),
-        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=20)).expect(2),
         call_enqueue("id_verification", 1, iso_ts(delta_minutes=10)).expect(2),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=20)).expect(3),
 
         call_dequeue().expect("id_verification", 1),
         call_dequeue().expect("bank_statements", 1),
         call_dequeue().expect("companies_house", 1),
     ])
+
 
 
 
