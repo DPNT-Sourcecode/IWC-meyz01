b7cb48228cc6bc33ada52b0b1d7c369c2b94b864
sbx_user1051 <sbx_user1051@169.254.42.125>
2026-02-08 15:04:41 +0000

    Sun Feb 08 15:04:41 UTC 2026

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,9 @@
 
 class SumSolution:
     
-    def compute(self, x, y):
-        raise NotImplementedError()
+    def compute(self, x: int, y: int):
+        if type(x) is not int or type(y) is not int:
+            raise TypeError("Incorrect type")
+        
+        return x + y
+
